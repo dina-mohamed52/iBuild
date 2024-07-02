65ba1325cd6ba03e910cebd567273a34c7bdc1ff
Dina Mohamed <66019870+dina-mohamed52@users.noreply.github.com>
2024-07-02 16:21:42 +0300

    Update ServicesCards.jsx

--- src/component/Services/ServicesCards.jsx
+++ src/component/Services/ServicesCards.jsx
@@ -188,7 +188,7 @@ function ServicesCards() {
           <div
             className={` ${styles.icon3}  ${
               isArabic
-                ? "absolute sm:mt-[-20rem] mt-[-12rem] sm:mb-[4rem] mb-[4rem] "
+                ? "absolute sm:mt-[-30rem] mt-[-20rem] sm:mb-[4rem] mb-[4rem] "
                 : ""
             }   sm:mt-[-3rem] mt-[5.5rem]   sm:mb-[1rem] mb-[0rem]   `}
           >
